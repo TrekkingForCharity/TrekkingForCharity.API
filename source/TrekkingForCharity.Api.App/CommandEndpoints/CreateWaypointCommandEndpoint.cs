1165388129d08a1320986d010d8ba8657f61fe35
Andrew Davis <andrew@deviouscreation.com>
2018-08-30 17:29:46 +0100

    Converting CreateWaypointCommandExecutor to an endpoint

---
+++ source/TrekkingForCharity.Api.App/CommandEndpoints/CreateWaypointCommandEndpoint.cs
@@ -0,0 +1,64 @@
+﻿// Copyright 2017 Trekking for Charity
+// This file is part of TrekkingForCharity.Api.
+// TrekkingForCharity.Api is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// TrekkingForCharity.Api is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with TrekkingForCharity.Api. If not, see http://www.gnu.org/licenses/.
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage.Table;
+using TrekkingForCharity.Api.App.Helpers;
+using TrekkingForCharity.Api.App.Infrastructure;
+using TrekkingForCharity.Api.Write.CommandExecutors;
+using TrekkingForCharity.Api.Write.Commands;
+using TrekkingForCharity.Api.Write.CommandValidators;
+
+namespace TrekkingForCharity.Api.App.CommandEndpoints
+{
+    public static class CreateWaypointCommandEndpoint
+    {
+        [FunctionName("CreateWaypointCommandEndpoint")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Admin, "post", Route = "commands/create-waypoint")]
+            HttpRequestMessage req,
+            [Table("trek")] CloudTable trekTable,
+            [Table("waypoint")] CloudTable waypointTable,
+            TraceWriter log,
+            ExecutionContext context)
+        {
+            try
+            {
+                var config = context.GenerateConfigurationRoot();
+
+                var currentUserAccessor = new CurrentUserAccessor(config, req);
+
+                var validator = new CreateWaypointCommandValidator();
+
+                var cmd = await req.GetCommand<CreateWaypointCommand>();
+
+                var executor =
+                    new CreateWaypointCommandExecutor(validator, trekTable, currentUserAccessor, waypointTable);
+
+                var validationResult = await executor.ValidateAndSetCommand(cmd);
+                if (!validationResult.IsValid)
+                {
+                    return req.CreateApiErrorResponseFromValidationResults(validationResult);
+                }
+
+                var result = await executor.Execute();
+
+                return req.CreateResponseMessageFromExecutionResult(result);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+                return req.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
